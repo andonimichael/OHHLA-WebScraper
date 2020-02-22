e567d494ec1f87897cb6bd5f6b4c5f26969a2b9a
Andoni Michael <andoni.m.garcia@gmail.com>
2020-02-22 00:00:48 -0800

    Create one file per artist
    Having all artists' lyrics in a single file was pretty ambitious. This
    file grows ridiculously large quite quickly. Instead, break out one
    output file per artist. As such, change the --output flag to indicate
    the output directory.
    
    NOTE: This implementation will re-write each artists text file on each
    run, but not necessarily clear the whole folder.

--- OHHLAScraper.py
+++ OHHLAScraper.py
@@ -2,7 +2,9 @@
 
 import urllib.request
 import re
+
 from lxml import html
+from pathlib import Path
 
 from argparser import create_arg_parser
 from sanitizer import clean_lyrics
@@ -11,7 +13,7 @@ ohhla = "http://ohhla.com/"
 sites = ["http://ohhla.com/all.html", "http://ohhla.com/all_two.html", "http://ohhla.com/all_three.html", "http://ohhla.com/all_four.html", "http://ohhla.com/all_five.html"];
 
 
-def handle_front_page(fpage, base, myFile):
+def handle_front_page(fpage, base, output_directory):
     artists = fpage.xpath("//pre/a[@href]/@href")
     for href in artists:
         if href == '':
@@ -19,15 +21,20 @@ def handle_front_page(fpage, base, myFile):
         elif href == 'anonymous/113/':	#A French Artist
             continue
         url = base + href
+        artist_name = href.rsplit('/')[-2]
+        artist_file_name = output_directory + '/' + artist_name + '.txt'
         try:
             ApageOpen = urllib.request.urlopen(url)
         except:
             continue
         ApageDom = html.fromstring(ApageOpen.read())
-        handle_artist_page(ApageDom, url, href, myFile)
+
+        with open(artist_file_name, 'w') as output_file:
+            handle_artist_page(ApageDom, url, href, output_file)
+            output_file.write('\n')
 
 
-def handle_artist_page(apage, base, nextstop, myFile):
+def handle_artist_page(apage, base, nextstop, output_file):
     albums = apage.xpath("//tr/td/a[@href]/@href")
     for href in albums:
         if href == '':
@@ -40,10 +47,10 @@ def handle_artist_page(apage, base, nextstop, myFile):
         except:
             continue
         AlpageDom = html.fromstring(AlpageOpen.read())
-        handle_album_page(AlpageDom, url, nextstop, myFile)
+        handle_album_page(AlpageDom, url, nextstop, output_file)
 
 
-def handle_album_page(alpage, base, stop, myFile):
+def handle_album_page(alpage, base, stop, output_file):
     currstop = "/" + stop
     songs = alpage.xpath("//tr/td/a[@href]/@href")
     for href in songs:
@@ -60,32 +67,34 @@ def handle_album_page(alpage, base, stop, myFile):
         TestAgainst = str(SpageDom)
         if re.match(r'^b[\'\"]<!DOCTYPE.*?>', TestAgainst) is not None:
             SpageHTML = html.fromstring(SpageDom)
-            handle_song_page(SpageHTML, myFile)
+            handle_song_page(SpageHTML, output_file)
         else:
             SpageDomUTF = SpageDom.decode("utf-8", "ignore")
             SpageClean = clean_lyrics(SpageDomUTF)
-            myFile.write(SpageClean)
+            output_file.write(SpageClean)
+            output_file.write('\n')
 
 
-def handle_song_page(spage, myFile):
+def handle_song_page(spage, output_file):
     try:
         lyrics = spage.xpath("//pre/text()")[0]
     except:
         return
     cleanup = clean_lyrics(lyrics)
-    myFile.write(cleanup)
+    output_file.write(cleanup)
+    output_file.write('\n')
 
 
-def scrape_OHHLA(output_file):
-    myFile = open(output_file, "w")
+def scrape_OHHLA(output_directory):
     for url in sites:
         fpageOpen = urllib.request.urlopen(url)
         fpageDom = html.fromstring(fpageOpen.read())
-        handle_front_page(fpageDom, ohhla, myFile)
-    myFile.close()
+        handle_front_page(fpageDom, ohhla, output_directory)
 
 
 if __name__ == '__main__':
     parser = create_arg_parser()
     args = parser.parse_args()
-    scrape_OHHLA(args.output_file)
+
+    Path(args.output_directory).mkdir(exist_ok=True)
+    scrape_OHHLA(args.output_directory)
--- argparser.py
+++ argparser.py
@@ -2,12 +2,12 @@ from argparse import ArgumentParser
 
 
 def create_arg_parser():
-    """ Creates an argument parser that handles output file names. """
+    """ Creates an argument parser that handles an output directory name. """
 
     cmd_line_parser = ArgumentParser()
     cmd_line_parser.add_argument('-o',
                                  '--output',
-                                 dest='output_file',
-                                 help='The output file',
-                                 default='lyrics.txt')
+                                 dest='output_directory',
+                                 help='The output directory',
+                                 default='lyrics')
     return cmd_line_parser
