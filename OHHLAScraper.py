cd1f30a16d42b733953054801163c2c50fc1f1ab
Andoni Michael <andoni.m.garcia@gmail.com>
2020-02-21 23:34:45 -0800

    Prefer snakecase in method names

--- OHHLAScraper.py
+++ OHHLAScraper.py
@@ -8,27 +8,27 @@ ohhla = "http://ohhla.com/"
 sites = ["http://ohhla.com/all.html", "http://ohhla.com/all_two.html", "http://ohhla.com/all_three.html", "http://ohhla.com/all_four.html", "http://ohhla.com/all_five.html"]; 
 
 
-def removeHeader(lyrics):
+def remove_header(lyrics):
     subHeader = re.sub(r'^([aA]rtist:|[aA]lbum:|[sS]ong:|[tT]yped [bB]y:).*?\n', '', lyrics, flags = re.MULTILINE)
     subFeatured = re.sub(r'^( |\t)*?f/.*?\n', '', subHeader, flags = re.MULTILINE)
     return subFeatured
 
 
-def removePreLyrics(lyrics):
+def remove_pre_lyrics(lyrics):
     subPreLyrics = re.sub(r'(\[(DJ|Prolouge|Intro|Mr\. Mixx).*?\]|-=talking=-|[iI]ntro:|Note:).*?(\n{2,})', '', lyrics, flags = re.DOTALL);  #Mr. Mixx for 2-Live Crew lyrics
     subPreLyrics2 = re.sub(r'(\[(DJ|Prolouge|Intro).*?\]|-=talking=-).*?\[', '[', subPreLyrics, flags = re.DOTALL);  #Catches poorly formatted pages
     subEquals = re.sub(r'^(\w*?|[^\s\w] [^\s\w]) ?= ?.*?\n', '', subPreLyrics2, flags = re.MULTILINE)
     return subEquals
 
 
-def removeChorus(lyrics):
+def remove_chorus(lyrics):
     sub10sionnotsinging = re.sub(r'\[10sion not singing\]', '', lyrics);  #Special Case for 10sion lyrics
     subChorus = re.sub(r'(\[(Chorus| CHORUS)[^\n]*?\]|Chorus:|Chorus( |\t)*?(\([^\n]*?\)|\{[^\n]*?\}):).*?(\n{2,}|$)', '', sub10sionnotsinging, flags = re.DOTALL)
     subChorus2 = re.sub(r'(\[(Chorus| CHORUS)[^\n]*?\]|Chorus:|Chorus( |\t)*?(\([^\n]*?\)|\{[^\n]*?\}):).*?\[', '[', subChorus, flags = re.DOTALL);  #Catches poorly formatted pages(CHECK IF ALL ARE NEEDED)
     return subChorus2
 
 
-def removeRest(lyrics):
+def remove_rest(lyrics):
     subSpecialCases = re.sub(r'(\[Nfamous\]\nClap your hands|\[John Legend\]\nI lay awake|\[Mike Posner\]\nGirl I\'m on that).*?$', '', lyrics, flags = re.DOTALL);  #Special Case for 1200Tech & (2) 2-Chaiz lyrics
     subNonLyricTags = re.sub(r'(\[(Interlude|Hook|[sS]cratches|Outro| OUTRO|[rR]epeat|Breakdown).*?\]|(Interlude:|Hook:|[sS]cratches:|Outro[^\n]*?:|[wW]oman\'s [vV]oice:|Luke[^\n]*?:|Breakdown[^\n]*?:|News [rR]eporter:|BILL[^\n]*?:|Clay D:)).*?(\n{2,}|$)', '', subSpecialCases, flags = re.DOTALL);  #Woman's voice, Luke, Breakdown, News reporter, & BILL: are for 2 Live Crew lyrics
     subNonLyricTags2 = re.sub(r'(\[(Interlude|Hook|[sS]cratches|Outro|[rR]epeat|Breakdown).*?\]|(Interlude:|Hook:|[sS]cratches:|Outro[^\n]*?:|Breakdown[^\n]*?:)).*?\[', '[', subNonLyricTags, flags = re.DOTALL);  #Catches poorly formatted pages
@@ -44,19 +44,19 @@ def removeRest(lyrics):
     return subRepeats
 
 
-def cleanCharacters(lyrics):
+def clean_characters(lyrics):
     subWeirdChar = re.sub(r'(~|\.{2,}|\+|\>|\<|\?(?=[^\n]))', '', lyrics);  #Leave in -{2,} because of blurred out curse words
     subEndings = re.sub(r'(!|\?){2,}', '\g<1>', subWeirdChar);  #Need to check ??? and !?!? cases
     subEndCommaHyph = re.sub(r'(\b,|\-)( |\t)*?(\n|$)', '\n', subEndings)
     return subEndCommaHyph
 
 
-def removeSingleWords(lyrics):
+def remove_single_words(lyrics):
     subOneWordLine = re.sub(r'^( |\t)*?\S+?( |\t)*?(\n|$)', '', lyrics, flags = re.MULTILINE)
     return subOneWordLine
 
 
-def removeWhitespace(lyrics):
+def remove_whitespace(lyrics):
     subSpaces = re.sub(r'^( |\t)+?(\b|\')', '\g<2>', lyrics, flags = re.MULTILINE)
     subEndSpaces = re.sub(r'( |\t)+?(\n|$)', '\n', subSpaces)
     subMidSpaces = re.sub(r'( |\t){2,}', ' ', subEndSpaces)
@@ -66,18 +66,18 @@ def removeWhitespace(lyrics):
     return subEndingLines
 
 
-def cleanLyrics(lyrics):
-    step1 = removeHeader(lyrics)
-    step2 = removePreLyrics(step1)
-    step3 = removeChorus(step2)
-    step4 = removeRest(step3)
-    step5 = cleanCharacters(step4)
-    step6 = removeSingleWords(step5)
-    step7 = removeWhitespace(step6)
+def clean_lyrics(lyrics):
+    step1 = remove_header(lyrics)
+    step2 = remove_pre_lyrics(step1)
+    step3 = remove_chorus(step2)
+    step4 = remove_rest(step3)
+    step5 = clean_characters(step4)
+    step6 = remove_single_words(step5)
+    step7 = remove_whitespace(step6)
     return step7
 
 
-def handleFrontPage(fpage, base, myFile):
+def handle_front_page(fpage, base, myFile):
     artists = fpage.xpath("//pre/a[@href]/@href")
     for href in artists:
         if href == '':
@@ -90,10 +90,10 @@ def handleFrontPage(fpage, base, myFile):
         except:
             continue
         ApageDom = html.fromstring(ApageOpen.read())
-        handleArtistPage(ApageDom, url, href, myFile)
+        handle_artist_page(ApageDom, url, href, myFile)
 
 
-def handleArtistPage(apage, base, nextstop, myFile):
+def handle_artist_page(apage, base, nextstop, myFile):
     albums = apage.xpath("//tr/td/a[@href]/@href")
     for href in albums:
         if href == '':
@@ -106,10 +106,10 @@ def handleArtistPage(apage, base, nextstop, myFile):
         except:
             continue
         AlpageDom = html.fromstring(AlpageOpen.read())
-        handleAlbumPage(AlpageDom, url, nextstop, myFile)
+        handle_album_page(AlpageDom, url, nextstop, myFile)
 
 
-def handleAlbumPage(alpage, base, stop, myFile):
+def handle_album_page(alpage, base, stop, myFile):
     currstop = "/" + stop
     songs = alpage.xpath("//tr/td/a[@href]/@href")
     for href in songs:
@@ -126,29 +126,29 @@ def handleAlbumPage(alpage, base, stop, myFile):
         TestAgainst = str(SpageDom)
         if re.match(r'^b[\'\"]<!DOCTYPE.*?>', TestAgainst) is not None:
             SpageHTML = html.fromstring(SpageDom)
-            handleSongPage(SpageHTML, myFile)
+            handle_song_page(SpageHTML, myFile)
         else:
             SpageDomUTF = SpageDom.decode("utf-8", "ignore")
-            SpageClean = cleanLyrics(SpageDomUTF)
+            SpageClean = clean_lyrics(SpageDomUTF)
             myFile.write(SpageClean)
 
 
-def handleSongPage(spage, myFile):
+def handle_song_page(spage, myFile):
     try:
         lyrics = spage.xpath("//pre/text()")[0]
     except:
         return
-    cleanup = cleanLyrics(lyrics)
+    cleanup = clean_lyrics(lyrics)
     myFile.write(cleanup)
 
 
-def scrapeOHHLA():
+def scrape_OHHLA():
     myFile = open("lyrics.txt", "w")
     for url in sites:
         fpageOpen = urllib.request.urlopen(url)
         fpageDom = html.fromstring(fpageOpen.read())
-        handleFrontPage(fpageDom, ohhla, myFile)
+        handle_front_page(fpageDom, ohhla, myFile)
     myFile.close()
 
 
-scrapeOHHLA()
+scrape_OHHLA()
