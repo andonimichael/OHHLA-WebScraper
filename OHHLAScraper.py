10eae6f9dfa84260775ac08a01f0ac085865451b
Andoni Michael <andoni.m.garcia@gmail.com>
2020-02-22 00:14:06 -0800

    Wrap the scraper in a class
    This allows us to constrain the API to just the scrape method,
    internally call some shared values instead of pass them throughout
    method chains, and encapsulate the business logic.

--- OHHLAScraper.py
+++ OHHLAScraper.py
@@ -3,7 +3,7 @@
 from pathlib import Path
 
 from argparser import create_arg_parser
-from scraper import scrape_OHHLA
+from scraper import OHHLAScraper
 
 OHHLA_URL = "http://ohhla.com/"
 ALL_ARTIST_SITES = ["http://ohhla.com/all.html",
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     Path(args.output_directory).mkdir(exist_ok=True)
-    scrape_OHHLA(OHHLA_URL, ALL_ARTIST_SITES, args.output_directory)
+    OHHLAScraper(OHHLA_URL, ALL_ARTIST_SITES, args.output_directory).scrape()
--- scraper.py
+++ scraper.py
@@ -5,80 +5,83 @@ from lxml import html
 from sanitizer import clean_lyrics
 
 
-def handle_front_page(fpage, base, output_directory):
-    artists = fpage.xpath("//pre/a[@href]/@href")
-    for href in artists:
-        if href == '':
-            continue
-        elif href == 'anonymous/113/':  # A French Artist
-            continue
-        url = base + href
-        artist_name = href.rsplit('/')[-2]
-        artist_file_name = output_directory + '/' + artist_name + '.txt'
+class OHHLAScraper:
+    def __init__(self, base_url, front_pages, output_directory):
+        self.base_url = base_url
+        self.front_pages = front_pages
+        self.output_directory = output_directory
+
+    def scrape(self):
+        for url in self.front_pages:
+            fpageOpen = urllib.request.urlopen(url)
+            fpageDom = html.fromstring(fpageOpen.read())
+            self._handle_front_page(fpageDom)
+
+    def _handle_front_page(self, fpage):
+        artists = fpage.xpath("//pre/a[@href]/@href")
+        for href in artists:
+            if href == '':
+                continue
+            elif href == 'anonymous/113/':  # A French Artist
+                continue
+            url = self.base_url + href
+            artist_name = href.rsplit('/')[-2]
+            artist_file_name = self.output_directory + '/' + artist_name + '.txt'
+            try:
+                ApageOpen = urllib.request.urlopen(url)
+            except:
+                continue
+            ApageDom = html.fromstring(ApageOpen.read())
+
+            with open(artist_file_name, 'w') as output_file:
+                self._handle_artist_page(ApageDom, url, href, output_file)
+                output_file.write('\n')
+
+    def _handle_artist_page(self, apage, base, nextstop, output_file):
+        albums = apage.xpath("//tr/td/a[@href]/@href")
+        for href in albums:
+            if href == '':
+                continue
+            elif href == '/anonymous/':
+                continue
+            url = base + href
+            try:
+                AlpageOpen = urllib.request.urlopen(url)
+            except:
+                continue
+            AlpageDom = html.fromstring(AlpageOpen.read())
+            self._handle_album_page(AlpageDom, url, nextstop, output_file)
+
+    def _handle_album_page(self, alpage, base, stop, output_file):
+        currstop = "/" + stop
+        songs = alpage.xpath("//tr/td/a[@href]/@href")
+        for href in songs:
+            if href == '':
+                continue
+            elif href == currstop:
+                continue
+            url = base + href
+            try:
+                SpageOpen = urllib.request.urlopen(url)
+            except:
+                continue
+            SpageDom = SpageOpen.read()
+            TestAgainst = str(SpageDom)
+            if re.match(r'^b[\'\"]<!DOCTYPE.*?>', TestAgainst) is not None:
+                SpageHTML = html.fromstring(SpageDom)
+                self._handle_song_page(SpageHTML, output_file)
+            else:
+                SpageDomUTF = SpageDom.decode("utf-8", "ignore")
+                SpageClean = clean_lyrics(SpageDomUTF)
+                output_file.write(SpageClean)
+                output_file.write('\n')
+
+    @staticmethod
+    def _handle_song_page(spage, output_file):
         try:
-            ApageOpen = urllib.request.urlopen(url)
+            lyrics = spage.xpath("//pre/text()")[0]
         except:
-            continue
-        ApageDom = html.fromstring(ApageOpen.read())
-
-        with open(artist_file_name, 'w') as output_file:
-            handle_artist_page(ApageDom, url, href, output_file)
-            output_file.write('\n')
-
-
-def handle_artist_page(apage, base, nextstop, output_file):
-    albums = apage.xpath("//tr/td/a[@href]/@href")
-    for href in albums:
-        if href == '':
-            continue
-        elif href == '/anonymous/':
-            continue
-        url = base + href
-        try:
-            AlpageOpen = urllib.request.urlopen(url)
-        except:
-            continue
-        AlpageDom = html.fromstring(AlpageOpen.read())
-        handle_album_page(AlpageDom, url, nextstop, output_file)
-
-
-def handle_album_page(alpage, base, stop, output_file):
-    currstop = "/" + stop
-    songs = alpage.xpath("//tr/td/a[@href]/@href")
-    for href in songs:
-        if href == '':
-            continue
-        elif href == currstop:
-            continue
-        url = base + href
-        try:
-            SpageOpen = urllib.request.urlopen(url)
-        except:
-            continue
-        SpageDom = SpageOpen.read()
-        TestAgainst = str(SpageDom)
-        if re.match(r'^b[\'\"]<!DOCTYPE.*?>', TestAgainst) is not None:
-            SpageHTML = html.fromstring(SpageDom)
-            handle_song_page(SpageHTML, output_file)
-        else:
-            SpageDomUTF = SpageDom.decode("utf-8", "ignore")
-            SpageClean = clean_lyrics(SpageDomUTF)
-            output_file.write(SpageClean)
-            output_file.write('\n')
-
-
-def handle_song_page(spage, output_file):
-    try:
-        lyrics = spage.xpath("//pre/text()")[0]
-    except:
-        return
-    cleanup = clean_lyrics(lyrics)
-    output_file.write(cleanup)
-    output_file.write('\n')
-
-
-def scrape_OHHLA(base_url, front_pages, output_directory):
-    for url in front_pages:
-        fpageOpen = urllib.request.urlopen(url)
-        fpageDom = html.fromstring(fpageOpen.read())
-        handle_front_page(fpageDom, base_url, output_directory)
+            return
+        cleanup = clean_lyrics(lyrics)
+        output_file.write(cleanup)
+        output_file.write('\n')
