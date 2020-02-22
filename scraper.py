f74fb355d4ea2c8080e66dc538fc6ba6b673b214
Andoni Michael <andoni.m.garcia@gmail.com>
2020-02-22 01:08:10 -0800

    Clean up scraper class
    - Rename methods to be more clear
    - Rename variables to be more clear
    - Pull out common and difficult logic
    - Simplify arguments being passed around

--- scraper.py
+++ scraper.py
@@ -6,6 +6,8 @@ from sanitizer import clean_lyrics
 
 
 class OHHLAScraper:
+    EXCLUDED_ARTISTS = {'113'}
+
     def __init__(self, base_url, front_pages, output_directory):
         self.base_url = base_url
         self.front_pages = front_pages
@@ -13,75 +15,82 @@ class OHHLAScraper:
 
     def scrape(self):
         for url in self.front_pages:
-            fpageOpen = urllib.request.urlopen(url)
-            fpageDom = html.fromstring(fpageOpen.read())
-            self._handle_front_page(fpageDom)
+            self._scrape_front_page(url)
 
-    def _handle_front_page(self, fpage):
-        artists = fpage.xpath("//pre/a[@href]/@href")
-        for href in artists:
-            if href == '':
+    def _scrape_front_page(self, url):
+        dom = self._extract_dom(url)
+        artist_refs = dom.xpath("//pre/a[@href]/@href")
+
+        for artist_ref in artist_refs:
+            ref_split = artist_ref.rsplit('/')
+            if not artist_ref or self._is_parent_ref(url, artist_ref) or len(ref_split) < 2:
                 continue
-            elif href == 'anonymous/113/':  # A French Artist
+
+            artist_name = ref_split[-2]
+            if not artist_name or artist_name in self.EXCLUDED_ARTISTS:
                 continue
-            url = self.base_url + href
-            artist_name = href.rsplit('/')[-2]
-            artist_file_name = self.output_directory + '/' + artist_name + '.txt'
-            try:
-                ApageOpen = urllib.request.urlopen(url)
-            except:
-                continue
-            ApageDom = html.fromstring(ApageOpen.read())
+
+            artist_url = self.base_url + artist_ref
+            artist_file_name = '{}/{}.txt'.format(self.output_directory, artist_name)
 
             with open(artist_file_name, 'w') as output_file:
-                self._handle_artist_page(ApageDom, url, href, output_file)
+                self._scrape_artist_page(artist_url, output_file)
                 output_file.write('\n')
 
-    def _handle_artist_page(self, apage, base, nextstop, output_file):
-        albums = apage.xpath("//tr/td/a[@href]/@href")
-        for href in albums:
-            if href == '':
-                continue
-            elif href == '/anonymous/':
-                continue
-            url = base + href
-            try:
-                AlpageOpen = urllib.request.urlopen(url)
-            except:
-                continue
-            AlpageDom = html.fromstring(AlpageOpen.read())
-            self._handle_album_page(AlpageDom, url, nextstop, output_file)
-
-    def _handle_album_page(self, alpage, base, stop, output_file):
-        currstop = "/" + stop
-        songs = alpage.xpath("//tr/td/a[@href]/@href")
-        for href in songs:
-            if href == '':
-                continue
-            elif href == currstop:
-                continue
-            url = base + href
-            try:
-                SpageOpen = urllib.request.urlopen(url)
-            except:
-                continue
-            SpageDom = SpageOpen.read()
-            TestAgainst = str(SpageDom)
-            if re.match(r'^b[\'\"]<!DOCTYPE.*?>', TestAgainst) is not None:
-                SpageHTML = html.fromstring(SpageDom)
-                self._handle_song_page(SpageHTML, output_file)
-            else:
-                SpageDomUTF = SpageDom.decode("utf-8", "ignore")
-                SpageClean = clean_lyrics(SpageDomUTF)
-                output_file.write(SpageClean)
-                output_file.write('\n')
-
-    @staticmethod
-    def _handle_song_page(spage, output_file):
+    def _scrape_artist_page(self, url, output_file):
         try:
-            lyrics = spage.xpath("//pre/text()")[0]
+            dom = self._extract_dom(url)
         except:
             return
-        cleanup = clean_lyrics(lyrics)
-        output_file.write(cleanup)
+
+        album_refs = dom.xpath("//tr/td/a[@href]/@href")
+        for album_ref in album_refs:
+            if not album_ref or self._is_parent_ref(url, album_ref):
+                continue
+            album_url = url + album_ref
+            self._scrape_album_page(album_url, output_file)
+
+    def _scrape_album_page(self, url, output_file):
+        try:
+            dom = self._extract_dom(url)
+        except:
+            return
+
+        song_refs = dom.xpath("//tr/td/a[@href]/@href")
+        for song_ref in song_refs:
+            if not song_ref or self._is_parent_ref(url, song_ref):
+                continue
+            song_url = url + song_ref
+            self._scrape_song_page(song_url, output_file)
+
+    def _is_parent_ref(self, url, ref):
+        start_of_relative_ref = len(self.base_url) - 1
+        end_of_relative_ref = url.rindex('/', 0, len(url) - 1) + 1
+        relative_ref = url[start_of_relative_ref:end_of_relative_ref]
+        return relative_ref == ref
+
+    @staticmethod
+    def _extract_dom(url):
+        opened_url = urllib.request.urlopen(url)
+        return html.fromstring(opened_url.read())
+
+    @staticmethod
+    def _scrape_song_page(url, output_file):
+        try:
+            opened_url = urllib.request.urlopen(url)
+            dom = opened_url.read()
+        except:
+            return
+
+        if re.match(r'^b[\'\"]<!DOCTYPE.*?>', str(dom)) is not None:
+            song_html = html.fromstring(dom)
+            try:
+                lyrics = song_html.xpath("//pre/text()")[0]
+            except:
+                return
+        else:
+            lyrics = dom.decode("utf-8", "ignore")
+
+        cleaned_lyrics = clean_lyrics(lyrics)
+        output_file.write(cleaned_lyrics)
         output_file.write('\n')
